ca1049c31e74e670653e638135f7c6456d22f35e
Rajrkjenkins <61007959+Rajrkjenkins@users.noreply.github.com>
2020-02-18 06:47:52 -0800

    Delete GetCredential.ps1

--- Jenkins-script/Motionsoft-Domain/New-VM script/GetCredential.ps1
+++
@@ -1,9 +0,0 @@
-﻿# This is used to generate encrypted format of Your USER-Name and Password
-
-$Credentials = Get-Credential
-$Credentials.Password | ConvertFrom-SecureString | Set-Content C:\Alok\password.txt # To Put Your location for File
-$Username = $Credentials.Username
-$Password = Get-Content “C:\Alok\password.txt” | ConvertTo-SecureString  # To give same location of above File
-$Credentials = New-Object System.Management.Automation.PSCredential $Username,$Password
-$Password = $Credentials.GetNetworkCredential().Password
-
